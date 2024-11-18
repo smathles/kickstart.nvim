2e31923f44ac8d30519d690c5ec236cb245adb98
Cameron Smith <114649266+smathles@users.noreply.github.com>
2024-11-18 16:31:59 +1000

    neotree mostly done :sunglasses:

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,8 @@
------------------------------------------------------------------------------- [[ Basic Keymaps ]]
+--  _  __
+-- | |/ /___ _  _ _ __  __ _ _ __ ___
+-- | ' </ -_) || | '  \/ _` | '_ (_-<
+-- |_|\_\___|\_, |_|_|_\__,_| .__/__/
+--           |__/           |_|
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -19,7 +23,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Add in shift+tab to dedent lines in insert mode
 vim.keymap.set('i', '<S-Tab>', '<C-\\><C-N><<<C-\\><C-N>^i')
 
--- Add ctrl+backspace to delete word in insert mode (heresy)
+-- Add ctrl+backspace to delete word in insert mode (heretic)
 vim.keymap.set('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
 
 -- Add in termguicolors for the bufferline plugin (they're also just good)
